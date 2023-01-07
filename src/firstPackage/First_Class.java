0f8f531d45e219c1c3148006067d6588761bf26f
georgenevinglovsky <114439488+georgenevinglovsky@users.noreply.github.com>
2023-01-07 11:32:24 -0800

    Update First_Class.java

--- src/firstPackage/First_Class.java
+++ src/firstPackage/First_Class.java
@@ -5,5 +5,7 @@ public class First_Class {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        //some changes
+        System.out.println("First Class");
     }
 }
